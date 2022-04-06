9f9dd459efbfb0ffcbbdeb8d6260530f22c064cb
김재훈 <junseak11051@gmail.com>
2022-04-06 22:10:03 +0900

    배포테스트

--- php/dbcon.php
+++ php/dbcon.php
@@ -25,13 +25,7 @@ $conn = mysqli_connect(
     'cloud_prj_jhk', // 비밀번호
     'cloud_classprj' // 데이터베이스 스키마
 );
-// $conn = mysqli_connect(
-//     'localhost',
-//     'root',
-//     '',
-//     'dgnr'
-// );
-// 한글깨짐 현상 관련
+
 mysqli_query($conn, "set session character_set_connection=utf8;");
 mysqli_query($conn, "set session character_set_results=utf8;");
 mysqli_query($conn, "set session character_set_client=utf8;");
