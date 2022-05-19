c68b2bedcb14a67ab7e60c0e0803d0d666ceef2e
juniorteam <juniorteam11@gmail.com>
2022-05-19 14:58:01 +0900

    로그아웃 오류 수정, 설정기능에 병합

--- app/src/main/java/com/classprj/myapplication/SettingActivity.java
+++ app/src/main/java/com/classprj/myapplication/SettingActivity.java
@@ -1,10 +1,13 @@
 package com.classprj.myapplication;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,6 +33,7 @@ public class SettingActivity extends AppCompatActivity {
         btnuser_logout = (Button) findViewById(R.id.btnuser_logout); //로그아웃
         btnuser_Withdrawal = (Button) findViewById(R.id.btnuser_Withdrawal); //계정탈퇴
 
+        //사용자 책 목록(삭제) 기능
         btnBook_list_setting.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -50,5 +54,22 @@ public class SettingActivity extends AppCompatActivity {
             }
         });
 
+        //로그아웃 기능
+        btnuser_logout.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //DB에 토큰으로 로그인 확인, SharedPreferences에 저장된 토큰 값을 빈칸" " 주면 해결
+                SharedPreferences pref = getSharedPreferences("login_session",MODE_PRIVATE);
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putString("token","");
+                editor.commit();
+
+                Toast.makeText(getApplicationContext(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+                Intent logoutintent = new Intent(SettingActivity.this, MainActivity.class);
+                startActivity(logoutintent);
+            }
+        });
+
+        
     }
 }
