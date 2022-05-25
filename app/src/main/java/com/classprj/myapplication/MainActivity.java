3fd81652097ff9a21b4d04394ceb08217c00d6a3
김재훈 <junseak11051@gmail.com>
2022-05-25 14:24:22 +0900

    동적 Listview완료

--- app/src/main/java/com/classprj/myapplication/MainActivity.java
+++ app/src/main/java/com/classprj/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -17,6 +18,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -30,13 +32,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -49,17 +60,34 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener {
 
 
     final private static String TAG = "tag";
+
     private static final int REQUEST_IMAGE_CAPTURE = 672;
     private String imageFilePath;
     private Uri photoUri;
-    ListView listView1;
-    ArrayAdapter<String> adapter;
-    ArrayList<String> listItem;
+
+
+    //listview 용
+    private static final String TAG_JSON="result";
+    private static final String TAG_ID = "book_id";
+    private static final String TAG_NAME = "book_name";
+
+    private TextView mTextViewResult;
+    ArrayList<HashMap<String, String>> mArrayList;
+    ListView mlistView;
+    String mJsonString;
+
+//    ListView listView1;
+//    ArrayAdapter<String> adapter;
+//    ArrayList<String> listItem;
 
     EditText editText1;
     Button button1;
@@ -84,9 +112,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             }
         }
 
+        //mTextViewResult = (TextView)findViewById(R.id.textView_main_result);
+        mlistView = (ListView) findViewById(R.id.listView1);
+        mArrayList = new ArrayList<>();
+
+        GetData task = new GetData();
+        task.execute("http://jhk.n-e.kr:8080/get_book_list.php");
 
 
-        listItem = new ArrayList<String>();
+        //listItem = new ArrayList<String>();
 
 
         // 아이템 추가한다.
@@ -94,17 +128,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             @Override
             public void onClick(View view) {
 
-                listItem.add(editText1.getText().toString());
-                adapter.notifyDataSetChanged(); // 변경되었음을 어답터에 알려준다.
-                editText1.setText("");
+//                listItem.add(editText1.getText().toString());
+//                adapter.notifyDataSetChanged(); // 변경되었음을 어답터에 알려준다.
+//                editText1.setText("");
             }
         });
 
 
-        adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,listItem);
-        listView1 = findViewById(R.id.listView1);
-        listView1.setAdapter(adapter);
-        listView1.setOnItemClickListener(this);
+//        adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,listItem);
+//        listView1 = findViewById(R.id.listView1);
+//        listView1.setAdapter(adapter);
+//        listView1.setOnItemClickListener(this);
 
 
     }//onCreate
@@ -208,8 +242,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                         }
                         if (which == 2){
                             System.out.println("++++"+which);
-                            listItem.remove(arg2);
-                            adapter.notifyDataSetChanged();
+                            //listItem.remove(arg2);
+                            //adapter.notifyDataSetChanged();
                         }
                         Toast.makeText(getApplicationContext(), title+":" +which+"."+classData[which], Toast.LENGTH_SHORT).show();
 
@@ -220,4 +254,134 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
+    //리스트뷰 가져오기 시작
+    private class GetData extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(MainActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            //mTextViewResult.setText(result);
+            Log.d(TAG, "response  - " + result);
+
+            if (result == null){
+
+                //mTextViewResult.setText(errorString);
+            }
+            else {
+
+                mJsonString = result;
+                showResult();
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String serverURL = params[0];
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.connect();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+
+        }
+    }
+
+
+    private void showResult(){
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+
+            for(int i=0;i<jsonArray.length();i++){
+
+                JSONObject item = jsonArray.getJSONObject(i);
+
+                String id = item.getString(TAG_ID);
+                String name = item.getString(TAG_NAME);
+
+                HashMap<String,String> hashMap = new HashMap<>();
+
+                hashMap.put(TAG_ID, id);
+                hashMap.put(TAG_NAME, name);
+
+                mArrayList.add(hashMap);
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, mArrayList, R.layout.item_list,
+                    new String[]{TAG_ID,TAG_NAME},
+                    new int[]{R.id.textView_list_id, R.id.textView_list_name}
+            );
+
+            mlistView.setAdapter(adapter);
+
+        } catch (JSONException e) {
+
+            Log.d(TAG, "showResult : ", e);
+        }
+
+    }
+//리스트뷰 가져오기 끝
 }
+
+
+
